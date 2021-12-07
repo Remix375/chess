24b4e1d0e33ac3cc81c32f316a5748fca8a5dc45
Remi Guillou <bigbossremi@gmail.com>
2021-12-07 22:32:08 +0100

    get queen movement

--- pieces/Dame/Dame.py
+++ pieces/Dame/Dame.py
@@ -4,3 +4,102 @@ from ..Piece import Piece
 class Dame(Piece):
     def __init__(self, color):
         super().__init__(color, "Dame")
+
+
+
+    #get all possible moves of the piece
+    def can_move(self, pos, grid):
+        possible = []
+
+                #right
+        x = pos[0]+1
+        y = pos[1]
+        while x <= 7:
+            if grid[y][x] == "":
+                possible.append((x,y))
+                x += 1
+            else:
+                break
+            
+        #left
+        x = pos[0]-1
+        y = pos[1]
+        while x >= 0:
+            if grid[y][x] == "":
+                possible.append((x,y))
+                x -= 1
+            else:
+                break
+            
+        
+        x = pos[0]
+        y = pos[1] + 1
+        #down
+        while y <= 7:
+            if grid[y][x] == "":
+                possible.append((x,y))
+                y += 1
+            else:
+                break
+
+
+
+        x = pos[0]
+        y = pos[1] - 1
+        #up
+        while y >= 0:
+            if grid[y][x] == "":
+                possible.append((x,y))
+                y -= 1
+            else:
+                break
+
+
+
+            
+        #top-right
+        x = pos[0] + 1
+        y = pos[1] - 1
+        while x <= 7 and y >= 0:
+            if grid[y][x] == "":
+                possible.append((x,y))
+                x += 1
+                y -= 1
+            else:
+                break
+            
+        #bottom-right
+        x = pos[0] + 1
+        y = pos[1] + 1
+        while x <= 7 and y <= 7:
+            if grid[y][x] == "":
+                possible.append((x,y))
+                x += 1
+                y += 1
+            else:
+                break
+            
+        #top-left
+        x = pos[0] - 1
+        y = pos[1] - 1
+        while y >= 0 and x >= 0:
+            if grid[y][x] == "":
+                possible.append((x,y))
+                x -= 1
+                y -= 1
+            else:
+                break
+
+
+        #bottom-left
+        x = pos[0] - 1
+        y = pos[1] + 1
+        while y <= 7 and x >= 0:
+            if grid[y][x] == "":
+                possible.append((x,y))
+                x -= 1
+                y += 1
+            else:
+                break
+
+        return possible
