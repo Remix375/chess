6547ba916f3a29214629145a75367073ebb28add
Remi Guillou <bigbossremi@gmail.com>
2021-12-12 15:44:12 +0100

    king movement

--- pieces/Roi/Roi.py
+++ pieces/Roi/Roi.py
@@ -6,5 +6,20 @@ class Roi(Piece):
         super().__init__(color, "Roi")
 
 
-    def can_move(self,  pos, grid):
-        
+    def can_move(self, pos, grid):
+        possible = []
+        x = pos[0]
+        y = pos[1]
+
+        for i in range(-1, 2):
+            for k in range(-1, 2):
+                current_x = x + i
+                current_y = y + k
+                if current_x <= 7 and current_x >= 0 and current_y <= 7 and current_y >= 0:
+                    if grid[current_y][current_x] == "":
+                        possible.append((current_x, current_y))
+                    else:
+                        if grid[current_y][current_x].color != self.color:
+                            possible.append((current_x, current_y))
+        return possible
+
