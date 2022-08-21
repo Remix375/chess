7e170e774978cc3239a4b81b1a77957fa7dd3f67
Remix375 <bigbossremi@gmail.com>
2022-08-21 14:46:24 +0800

    redone queen

--- pieces/Dame/Dame.py
+++ pieces/Dame/Dame.py
@@ -1,3 +1,4 @@
+from re import X
 import pygame
 from ..Piece import Piece
 
@@ -5,118 +6,116 @@ class Dame(Piece):
     def __init__(self, color):
         super().__init__(color, "Dame")
 
-
-
     #get all possible moves of the piece
     #movement method
     def can_move(self, pos, grid):
         possible = []
 
-                #right
-        x = pos[0]+1
-        y = pos[1]
+        #right
+        x = pos[1]+1
+        y = pos[0]
         while x <= 7:
             if grid[y][x] == "":
-                possible.append((x,y))
+                possible.append((y,x))
                 x += 1
             else:
                 if grid[y][x].color != self.color:
-                    possible.append((x,y))
+                    possible.append((y,x))
                 break
             
         #left
-        x = pos[0]-1
-        y = pos[1]
+        x = pos[1]-1
+        y = pos[0]
         while x >= 0:
             if grid[y][x] == "":
-                possible.append((x,y))
+                possible.append((y,x))
                 x -= 1
             else:
                 if grid[y][x].color != self.color:
-                    possible.append((x,y))
+                    possible.append((y,x))
                 break
             
         
-        x = pos[0]
-        y = pos[1] + 1
+        x = pos[1]
+        y = pos[0] + 1
         #down
         while y <= 7:
             if grid[y][x] == "":
-                possible.append((x,y))
+                possible.append((y,x))
                 y += 1
             else:
                 if grid[y][x].color != self.color:
-                    possible.append((x,y))
+                    possible.append((y,x))
                 break
 
 
 
-        x = pos[0]
-        y = pos[1] - 1
+        x = pos[1]
+        y = pos[0] - 1
         #up
         while y >= 0:
             if grid[y][x] == "":
-                possible.append((x,y))
+                possible.append((y,x))
                 y -= 1
             else:
                 if grid[y][x].color != self.color:
-                    possible.append((x,y))
+                    possible.append((y,x))
                 break
 
 
 
             
         #top-right
-        x = pos[0] + 1
-        y = pos[1] - 1
+        x = pos[1] + 1
+        y = pos[0] - 1
         while x <= 7 and y >= 0:
             if grid[y][x] == "":
-                possible.append((x,y))
+                possible.append((y,x))
                 x += 1
                 y -= 1
             else:
                 if grid[y][x].color != self.color:
-                    possible.append((x,y))
+                    possible.append((y,x))
                 break
             
         #bottom-right
-        x = pos[0] + 1
-        y = pos[1] + 1
+        x = pos[1] + 1
+        y = pos[0] + 1
         while x <= 7 and y <= 7:
             if grid[y][x] == "":
-                possible.append((x,y))
+                possible.append((y,x))
                 x += 1
                 y += 1
             else:
                 if grid[y][x].color != self.color:
-                    possible.append((x,y))
+                    possible.append((y,x))
                 break
             
         #top-left
-        x = pos[0] - 1
-        y = pos[1] - 1
+        x = pos[1] - 1
+        y = pos[0] - 1
         while y >= 0 and x >= 0:
             if grid[y][x] == "":
-                possible.append((x,y))
+                possible.append((y,x))
                 x -= 1
                 y -= 1
             else:
                 if grid[y][x].color != self.color:
-                    possible.append((x,y))
+                    possible.append((y,x))
                 break
 
 
         #bottom-left
-        x = pos[0] - 1
-        y = pos[1] + 1
+        x = pos[1] - 1
+        y = pos[0] + 1
         while y <= 7 and x >= 0:
             if grid[y][x] == "":
-                possible.append((x,y))
+                possible.append((y,x))
                 x -= 1
                 y += 1
             else:
                 if grid[y][x].color != self.color:
-                    possible.append((x,y))
+                    possible.append((y,x))
                 break
 
         return possible
