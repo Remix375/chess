f666fd4c5789da7d1fc6bac34ceb78a10e2bcdd3
Remix375 <bigbossremi@gmail.com>
2022-08-21 14:43:56 +0800

    redone bishop

--- pieces/Fou/Fou.py
+++ pieces/Fou/Fou.py
@@ -14,56 +14,56 @@ class Fou(Piece):
         #if empty or if ennemie piece we can go
 
         #top-right
-        x = pos[0] + 1
-        y = pos[1] - 1
+        x = pos[1] + 1
+        y = pos[0] - 1
         while x <= 7 and y >= 0:
             if grid[y][x] == "":
-                possible.append((x,y))
+                possible.append((y,x))
                 x += 1
                 y -= 1
             else:
                 if grid[y][x].color != self.color:
-                    possible.append((x,y))
+                    possible.append((y,x))
                 break
             
         #bottom-right
-        x = pos[0] + 1
-        y = pos[1] + 1
+        x = pos[1] + 1
+        y = pos[0] + 1
         while x <= 7 and y <= 7:
             if grid[y][x] == "":
-                possible.append((x,y))
+                possible.append((y,x))
                 x += 1
                 y += 1
             else:
                 if grid[y][x].color != self.color:
-                    possible.append((x,y))
+                    possible.append((y,x))
                 break
             
         #top-left
-        x = pos[0] - 1
-        y = pos[1] - 1
+        x = pos[1] - 1
+        y = pos[0] - 1
         while y >= 0 and x >= 0:
             if grid[y][x] == "":
-                possible.append((x,y))
+                possible.append((y,x))
                 x -= 1
                 y -= 1
             else:
                 if grid[y][x].color != self.color:
-                    possible.append((x,y))
+                    possible.append((y,x))
                 break
 
 
         #bottom-left
-        x = pos[0] - 1
-        y = pos[1] + 1
+        x = pos[1] - 1
+        y = pos[0] + 1
         while y <= 7 and x >= 0:
             if grid[y][x] == "":
-                possible.append((x,y))
+                possible.append((y,x))
                 x -= 1
                 y += 1
             else:
                 if grid[y][x].color != self.color:
-                    possible.append((x,y))
+                    possible.append((y,x))
                 break
 
         return possible
